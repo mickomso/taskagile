57fdec7716513fae1924051ef9e821948df591b1
Miguel Company Soler <miguelcompanysoler@gmail.com>
2020-05-08 13:23:53 +0200

    Bootstrap CSS files added

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   devServer: {
-    port:3000,
+    port: 3000,
     proxy: {
       '/api/*': {
         target: 'http://localhost:8080'
       }
+    }
+  },
+  configureWebpack: {
+    entry: {
+      app: './src/main.js',
+      style: ['bootstrap/dist/css/bootstrap.min.css']
     }
   }
 }
